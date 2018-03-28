48a8d3813586d3fcef29cdea12ad95cdb967a848
unknown <jjernigan1065@gmail.com>
2018-03-28 16:01:34 -0400

    finished logic, working on styling

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -28,8 +28,7 @@ $("#submit").on("click", function(event){
        train: train,
        dest: destination,
        firstTrain: firstTrain,
-       frequency: frequency,
-       timeAdded: moment().unix()
+       frequency: frequency
     });
     //logging the items to be pushed
     console.log("logging the push items");
@@ -38,26 +37,59 @@ $("#submit").on("click", function(event){
     console.log(firstTrain);
     console.log(frequency);
     console.log("--------------")
+    $("#trainName").val('');
+    $("#destination").val('');
+    $("#firstTime").val('');
+    $("#Frequency").val('');
 
 });
 //database event listener looking for new children objects added to the database
 data.ref().on("child_added", function(snapshot){
     //making the table body for the data to be added to
+    console.log(minutes);
+
     var tableBody = $("#t-body");
     //making the row for the data to go inside
     var tableRow = $("<tr>");
 
-    console.log("logging itmes from the database");
+    console.log("logging items from the database");
     console.log(snapshot.val().train);
     console.log(snapshot.val().dest);
     console.log(snapshot.val().firstTrain);
     console.log(snapshot.val().frequency);
-    console.log(snapshot.val().timeAdded);
+    // console.log(snapshot.val().curTime);
     console.log("----------------------");
 
-    tableRow.append("<th>" + snapshot.val().train + "</th>" +
-        "<th>" + snapshot.val().dest + "</th>" +
-        "<th>" + snapshot.val().frequency + " minutes" + "</th>");
+    //get the current time when the page is loaded,
+    //subtract the current time by the first train time then % by frequency to get how long until next train.
+    //add time until next train to the current time to show when the time the next train will arrive.
+    //take into account that after 60 minutes the hour starts again from 0
+    //append the value for the time and then the number of minutes until the next train.
+    var timeStamp = moment(snapshot.val().firstTrain, "h:mm");
+
+    console.log(timeStamp.toString());
+    var minutes = moment().diff(timeStamp, 'minutes') ;
+    console.log("this is the difference in minutes: " + minutes);
+    // if(minutes % snapshot.val().frequency === 0){
+    //     var minsAway = snapshot.val().frequency
+    // }else{
+    //     minsAway = minutes % snapshot.val().frequency;
+    //
+    // }
+    var tRemainder = minutes % snapshot.val().frequency;
+    var minsAway = snapshot.val().frequency - tRemainder;
+    console.log("this is the minutes away");
+    console.log(minsAway);
+
+    var nextTrain = moment().add(minsAway, "minutes");
+    var nextTime = moment(nextTrain, "h:mm").format("h:mm").toString();
+    console.log("this is the next train time: " + nextTime);
+
+    tableRow.append("<td>" + snapshot.val().train + "</td>" +
+        "<td>" + snapshot.val().dest + "</td>" +
+        "<td>" + snapshot.val().frequency + "</td>" +
+        "<td>" + nextTime + "</td>" +
+        "<td>" + minsAway + "</td>");
     tableBody.append(tableRow);
 
 
